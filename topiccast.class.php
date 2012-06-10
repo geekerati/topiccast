103390e0abce0142ce855a83bfafb8801f5607f7
BNU <bnufactory@gmail.com>
2012-06-10 22:04:16 +0900

    스킨 경로 바로 잡음. info.xml 추가.

--- topiccast.class.php
+++ topiccast.class.php
@@ -4,7 +4,7 @@ class topiccast extends WidgetHandler
 
 	function proc($args)
 	{
-		$tpl_path = sprintf('%stpl', $this->widget_path);
+		$tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
 		$tpl_file = 'skin';
 		$oTemplate = &TemplateHandler::getInstance();
 		return $oTemplate->compile($tpl_path, $tpl_file);
