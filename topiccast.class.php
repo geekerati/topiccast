0b78a199a0bf7786cef5b0faec1821c20d61e9b9
BNU <bnufactory@gmail.com>
2012-06-11 12:36:43 +0900

    cast_id 설정 및 정보 가져오기 추가.

--- topiccast.class.php
+++ topiccast.class.php
@@ -1,9 +1,14 @@
 <?php
 class topiccast extends WidgetHandler
 {
-
 	function proc($args)
 	{
+		$oTopicfluxModel = &getModel('topicflux');
+
+		// cast 정보
+		$castInfo = $oTopicfluxModel->getCastInfo($args->cast_id);
+		debugPrint($castInfo);
+
 		$tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
 		$tpl_file = 'skin';
 		$oTemplate = &TemplateHandler::getInstance();
